9a09017a959be29fe733d2ffe96c104673871cd7
Rishabh Raj <rrishabh1800@gmail.com>
2018-10-01 03:34:11 +0530

    Initial Commit

---
+++ Podacasts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podacasts
+//
+//  Created by Rishabh Raj on 01/10/18.
+//  Copyright © 2018 Rishabh Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
