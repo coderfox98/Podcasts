8a76d1997460892d82bd89626929e582ade8db4c
Rishabh Raj <rrishabh1800@gmail.com>
2018-10-01 11:30:15 +0530

    Initial setup

--- Podacasts/MainTabBarController.swift
+++ Podacasts/MainTabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabBarController.swift
 //  Podacasts
 //
 //  Created by Rishabh Raj on 01/10/18.
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainTabBarController : UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .red
     }
-
-
 }
-
